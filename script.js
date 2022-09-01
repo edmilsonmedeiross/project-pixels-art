2cbd323a6fb1e11379daf470c5a7e5195bc2937a
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-09-01 13:18:20 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-function generateColor () {
+function generateColor() {
   let r = parseInt(Math.random() * 255);
   let g = parseInt(Math.random() * 255);
   let b = parseInt(Math.random() * 255);
-  let color = '(' + r + ', ' + g + ', ' + b + ')'
+  let color = `rgb(${r}, ${g}, ${b})`;
+
   return color;
 }
 
-function generateColor1 () {
-  let r = parseInt(Math.random() * 255);
-  let g = parseInt(Math.random() * 255);
-  let b = parseInt(Math.random() * 255);
-  let color = '(' + r + ', ' + g + ', ' + b + ')'
-  return color;
+function trocaColor() {
+  let bccolor = 0;
+  for (let index = 1; index < 4; index += 1) {
+    document.getElementById(`color-${index + 1}`).style.backgroundColor = generateColor();
+  }
+  console.log(bccolor);
+  return bccolor;
 }
 
-function generateColor2 () {
-  let r = parseInt(Math.random() * 255);
-  let g = parseInt(Math.random() * 255);
-  let b = parseInt(Math.random() * 255);
-  let color = '(' + r + ', ' + g + ', ' + b + ')'
-  return color;
-}
-
-const colorRed = document.getElementById('red');
-const colorBlue = document.getElementById('blue');
-const colorGreen = document.getElementById('green');
+const colorRed = document.getElementById('color-2');
+const colorBlue = document.getElementById('color-3');
+const colorGreen = document.getElementById('color-4');
 const containerButton = document.getElementById('button-random-color');
 //console.log (containerButton)
 
-containerButton.addEventListener('click', generateColor, generateColor1, generateColor2);
+containerButton.addEventListener('click', trocaColor);
